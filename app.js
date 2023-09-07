031f234fcce35ead1660b7012a8e008bada8cb9c
bilal muili <muilibilal2019@gmail.com>
2023-09-07 20:06:13 +0100

    made changes

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const timeEl = document.querySelector("[data-testid='currentUTCTime'] span");
 const dayEl = document.querySelector(
   "[data-testid='currentDayOfTheWeek'] span"
 );
+const imgCont = document.querySelector(".profile-logo");
+const loaderEl = document.querySelector(".profile-logo span");
+const imageEl = document.querySelector(".profile-logo img");
 
 // Intl Date format
 let formatting = (options) => {
   return new Intl.DateTimeFormat("en-US", options).format(new Date());
 };
 
+function removeLoader() {
+  imgCont.style.display = "flex";
+  imageEl.style.display = "flex";
+  loaderEl.style.display = "none";
+}
+
 setInterval(() => {
-  timeEl.innerText = formatting({
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  });
+  timeEl.innerText = new Date().getTime();
 }, 1000);
 
 dayEl.innerText = formatting({
@@ -22,4 +27,8 @@ dayEl.innerText = formatting({
   year: "numeric",
   month: "long",
   day: "numeric",
+});
+
+imageEl.addEventListener("load", () => {
+  removeLoader();
 });
